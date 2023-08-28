e4d4db9112d2fc71dc6b581765fb7117ad8d9356
Jyan Zarate <git@jcharante.com>
2023-08-28 21:02:58 +0700

    remove pocketbase code

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 'use strict'
 
 import express from 'express';
-const PocketBase = require('pocketbase/cjs')
 const bcrypt = require('bcrypt')
 import uuid4 from 'uuid4'
 import { errorWrapper, getUserWrapper, requiresValidSessionKeyWrapper, requireIsRestaurantOwnerWrapper } from './wrappers'
@@ -25,60 +24,6 @@ import * as controller from './controller/controller'
 import {getAddonCategories, postAddAddonToCategory, postCreateAddon} from "./controller/restaurant/food/addons";
 
 
-class PBSingleton {
-    private static instance: PBSingleton;
-    private pbClient: any;
-
-    private constructor() {
-    }
-
-    public static getInstance(): PBSingleton {
-        if (!PBSingleton.instance) {
-            PBSingleton.instance = new PBSingleton();
-        }
-        return PBSingleton.instance;
-    }
-
-    public async getClient(): Promise<typeof PocketBase> {
-        if (!this.pbClient) {
-            this.pbClient = new PocketBase('http://127.0.0.1:8090')
-            // @ts-ignore
-            const adminData = await this.pbClient.admins.authViaEmail(process.env.PB_EMAIL, process.env.PB_PASSWORD)
-        }
-        return this.pbClient
-    }
-}
-
-
-interface PBCollectionResponse {
-    page: number,
-    perPage: number,
-    totalItems: number,
-    totalPages: number,
-}
-
-interface PBPosition {
-    latitude: number,
-    longitude: number
-}
-
-interface PBCollectionRestaurant {
-    id: string,
-    created: string,
-    updated: string,
-    name: string,
-    owner: string,
-    inventoryManagers: string[],
-    isVisible: boolean,
-    isVerified: boolean,
-    address: string,
-    city: string,
-    position: PBPosition | null,
-}
-
-interface PBCollectionRestaurantsListResponse extends PBCollectionResponse {
-    items: PBCollectionRestaurant[]
-}
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Goodies API');
