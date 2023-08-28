b49a6546fdda5024273714a2a009cce0e56ec4c8
Jyan Zarate <git@jcharante.com>
2023-08-28 21:02:58 +0700

    add basic auth endpoints

--- index.ts
+++ index.ts
@@ -1,13 +1,70 @@
 'use strict'
 
 import express from 'express';
-import {stringify} from "querystring";
+import { Schema, model, connect, CallbackError } from 'mongoose';
+import * as mongoose  from 'mongoose'
 const PocketBase = require('pocketbase/cjs')
+const bcrypt = require('bcrypt')
+import uuid4 from 'uuid4'
 
 let adminTokenObtained = false
 let adminToken = ''
 
 const app = express();
+app.use(express.json());
+
+
+interface IUserV1 {
+    collectionInterface: string,
+    canonical_id: string,
+    name: string,
+    email?: string,
+    phone?: string,
+    password?: string
+}
+
+const UserV1Schema = new Schema<IUserV1>({
+    collectionInterface: { type: String, default: 'UserV1' },
+    canonical_id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String },
+    phone: { type: String },
+    password: { type: String }
+});
+
+const UserV1 = model<IUserV1>('UserV1', UserV1Schema);
+
+interface ISessionKeyV1 {
+    _id: mongoose.Types.ObjectId,
+    collectionInterface: string,
+    canonical_id: string
+}
+
+const SessionKeyV1Schema = new Schema<ISessionKeyV1>({
+    collectionInterface: { type: String, default: 'SessionKeyV1' },
+    canonical_id: { type: String, required: true }
+})
+
+const SessionKeyV1 = model<ISessionKeyV1>('SessionKeyV1', SessionKeyV1Schema)
+
+class MongoDBSingleton {
+    private static instance: MongoDBSingleton;
+    private constructor() { }
+
+    public static async getInstance() {
+        if (!MongoDBSingleton.instance) {
+            console.log('Creating instance of MongoDBSingleton')
+            MongoDBSingleton.instance = new MongoDBSingleton();
+            mongoose.set('bufferCommands', false);
+            try {
+                await connect('mongodb://0.0.0.0:27017/goodies?directConnection=true')
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        return MongoDBSingleton.instance;
+    }
+}
 
 class PBSingleton {
     private static instance: PBSingleton;
@@ -33,23 +90,6 @@ class PBSingleton {
     }
 }
 
-const getAdminToken = async (): Promise<string> => {
-    if (!adminTokenObtained) {
-        const res = await fetch('http://127.0.0.1:8090/api/admins/auth-via-email', {
-            method: 'POST',
-            body: JSON.stringify({ 'email': process.env.PB_EMAIL, 'password': process.env.PB_PASSWORD }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        console.log(res)
-        const { token } = await res.json()
-        console.log(token)
-        adminToken = token
-        adminTokenObtained = true
-    }
-    return adminToken
-}
 
 interface PBCollectionResponse {
     page: number,
@@ -105,6 +145,99 @@ app.get('/restaurants', async (req, res) => {
         return restaurant
     })
     res.send(body)
+})
+
+app.get('/users', async (req, res) => {
+    await MongoDBSingleton.getInstance()
+
+    const users: IUserV1[] = await UserV1.find({})
+
+    res.send(users)
+})
+
+app.post('/login/email', async (req, res) => {
+    try {
+        await MongoDBSingleton.getInstance()
+
+        const user = await UserV1.findOne({ email: req.body.email })
+
+        if (!user) {
+            res.status(404).send('Email not found')
+            return
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+
+        if (!passwordMatch) {
+            res.status(401).send('Incorrect password')
+            return
+        }
+
+        const sessionKey = await SessionKeyV1.create({ canonical_id: uuid4() })
+
+        await sessionKey.save()
+
+        res.send({
+            sessionKey: sessionKey._id
+        })
+    } catch(err) {
+        console.error(err)
+        res.status(500).send('Server error')
+    }
+})
+
+app.delete('/logout', async (req, res) => {
+    try {
+        // get bearer token from header
+        if (!req.headers.authorization) {
+            res.status(401).send('Unauthorized')
+            return
+        }
+        if (!req.headers.authorization.startsWith('Bearer ')) {
+            res.status(401).send('Unauthorized')
+            return
+        }
+        const bearerToken = req.headers.authorization?.split(' ')[1]
+
+        await MongoDBSingleton.getInstance()
+
+        const sessionKey = await SessionKeyV1.findById(bearerToken)
+
+        if (sessionKey) {
+            await sessionKey.delete()
+        }
+        res.status(200).send('OK')
+    } catch (err) {
+        console.error(err)
+        res.send(500).send('Server error')
+    }
+})
+
+app.post('/signup/email', async (req, res) => {
+    try {
+        const body = req.body;
+
+        if (!(body.email && body.password)) {
+            return res.status(400).send({error: "Data not formatted properly"});
+        }
+
+        await MongoDBSingleton.getInstance()
+
+        const user = new UserV1({
+            email: body.email,
+            name: body.name,
+            canonical_id: body.email // todo: generate this maybe?
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(body.password, salt);
+
+        await user.save()
+        res.status(200).send('ok')
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Server error')
+    }
 })
 
 require('dotenv').config({ path: '.env.local' })
